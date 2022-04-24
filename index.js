5dd5dab1d29cd9fc51787942ffd2dc9888690228
Rebai Souha <souharebai111@gmail.com>
2022-04-24 03:27:23 +0100

    full functional video chat

---
+++ index.js
@@ -0,0 +1,36 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const cors = require("cors"); // cross origin request
+
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "*", //accessform all origin
+		methods: [ "GET", "POST" ]
+	}
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+	res.send('Running');
+});
+
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id);
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	});
+
+	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+	});
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	});
+});
+
+server.listen(PORT, () => console.log(`Le serveur a demarré sur le port ${PORT}`));
